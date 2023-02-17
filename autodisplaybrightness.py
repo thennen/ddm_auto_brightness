bc5beb73ca9325bcc55bf60d0b9c1500f399c915
Tyler Hennen <tyler@hennen.us>
2023-02-17 08:43:29 +0100

    Adjust for actual time of sunrise and sunset during the year

--- autodisplaybrightness.py
+++ autodisplaybrightness.py
@@ -2,18 +2,31 @@
 # just put it in the windows scheduler
 
 import os
-from math import cos, pi
+from math import sin, cos, pi
 from datetime import datetime
+from dateutil import tz
 
-# is a sin function a good idea?
-maxbrightness = 70
-minbrightness = 10
-now = datetime.now()
 
-nowDec = now.hour + now.minute/60
+maxbrightness = 80
+minbrightness = 5
+now = datetime.now(tz.tz.tzlocal())
+try:
+    from suntime import Sun
+    sun = Sun(50.7753, 6.0839)
+    rise = sun.get_local_sunrise_time()
+    set = sun.get_local_sunset_time()
+    dayfrac = (now - rise) / (set - rise)
+    dayfrac = max(dayfrac, 0)
+    dayfrac = min(dayfrac, 1)
+    # always at minbrightness while sun is set
+    brightness = int(round(minbrightness + (maxbrightness - minbrightness) * sin(dayfrac*pi)))
+except:
+    # old way
+    nowDec = now.hour + now.minute/60
+    maxtime = 12
+    # is a sin function a good idea?
+    brightness = int(round(minbrightness + (maxbrightness - minbrightness) * (1 + cos((nowDec - maxtime)*2*pi/24))/2))
 
-maxtime = 12
-
-brightness = int(round(minbrightness + (maxbrightness - minbrightness) * (1 + cos((nowDec - maxtime)*2*pi/24))/2))
-
-os.system(fr'"C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /SetBrightnessLevel {brightness}')
+bypass = False
+if not bypass:
+    os.system(fr'"C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /SetBrightnessLevel {brightness}')
